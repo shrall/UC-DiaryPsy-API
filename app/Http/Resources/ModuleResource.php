ecded146441e66633a8cce74d9e3f8c158fc9a84
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-07-10 19:30:27 +0700

    feat(admin): module resource

---
+++ app/Http/Resources/ModuleResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ModuleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "status" => $this->status,
+            "order" => $this->order,
+            "path" => $this->path,
+            "color_hex" => $this->color_hex,
+            "characters" => $this->characters
+        ];
+    }
+}
